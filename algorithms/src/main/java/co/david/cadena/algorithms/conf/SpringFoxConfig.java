f2b8255463ac15694dba92325efe438e710b2ff3
david.cadena <david.cadena@imac-de-david>
2020-04-22 17:44:02 -0500

    Swagger update to 3.0.0

--- algorithms/src/main/java/co/david/cadena/algorithms/conf/SpringFoxConfig.java
+++ algorithms/src/main/java/co/david/cadena/algorithms/conf/SpringFoxConfig.java
@@ -4,25 +4,27 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Import;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class SpringFoxConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework")))
-				.paths(PathSelectors.any()).build().apiInfo(getApiInfo());
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("co.david.cadena")).paths(PathSelectors.any()).build()
+				.apiInfo(getApiInfo());
 	}
 
 	private ApiInfo getApiInfo() {
