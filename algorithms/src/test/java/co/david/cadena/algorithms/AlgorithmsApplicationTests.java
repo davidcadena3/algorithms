a8ab7580f4f31d7c69ca99b95e38b72c5ddf45ce
david.cadena <david.cadena@imac-de-david>
2020-04-22 16:33:23 -0500

    Simple search New tests!!

--- algorithms/src/test/java/co/david/cadena/algorithms/AlgorithmsApplicationTests.java
+++ algorithms/src/test/java/co/david/cadena/algorithms/AlgorithmsApplicationTests.java
@@ -15,19 +15,43 @@ import co.david.cadena.algorithms.dto.SearchResponseDTO;
 //github.com/davidcadena3/algorithms.git
 import co.david.cadena.algorithms.exception.AlgorithmsException;
 import co.david.cadena.algorithms.service.BinarySearchService;
+import co.david.cadena.algorithms.service.SimpleSearchService;
 
 @SpringBootTest
 class AlgorithmsApplicationTests {
 
 	@Autowired
-	private BinarySearchService binaryService;
+	private SimpleSearchService simpleSearchService;
+
+	@Autowired
+	private BinarySearchService binarySearchService;
 
 	private final String tId = UUID.randomUUID().toString();
 
 	@Test
+	void simpleSearchOk() throws AlgorithmsException {
+		SearchResponseDTO response = simpleSearchService.search(tId, new SearchRequestDTO(new int[] { 1, 2, 3, 4 }, 3));
+		assertNotNull(response);
+		assertEquals(2, response.getMatchPosition());
+	}
+
+	@Test
+	void simpleSearchNonOk() throws AlgorithmsException {
+		assertThrows(AlgorithmsException.class, () -> {
+			simpleSearchService.search(tId, new SearchRequestDTO(new int[] { 1, 2, 3, 4 }, 5));
+		});
+	}
+
+	@Test
+	void simpleSearchBadRequest() throws AlgorithmsException {
+		assertThrows(AlgorithmsException.class, () -> {
+			simpleSearchService.search(tId, new SearchRequestDTO());
+		});
+	}
+
+	@Test
 	void binarySearchOk() throws AlgorithmsException {
-		SearchResponseDTO response = binaryService.search(tId,
-				new SearchRequestDTO(new int[] { 1, 2, 3, 4 }, 3));
+		SearchResponseDTO response = binarySearchService.search(tId, new SearchRequestDTO(new int[] { 1, 2, 3, 4 }, 3));
 		assertNotNull(response);
 		assertEquals(2, response.getMatchPosition());
 	}
@@ -35,14 +59,14 @@ class AlgorithmsApplicationTests {
 	@Test
 	void binarySearchNonOk() throws AlgorithmsException {
 		assertThrows(AlgorithmsException.class, () -> {
-			binaryService.search(tId, new SearchRequestDTO(new int[] { 1, 2, 3, 4 }, 5));
+			binarySearchService.search(tId, new SearchRequestDTO(new int[] { 1, 2, 3, 4 }, 5));
 		});
 	}
 
 	@Test
 	void binarySearchBadRequest() throws AlgorithmsException {
 		assertThrows(AlgorithmsException.class, () -> {
-			binaryService.search(tId, new SearchRequestDTO());
+			binarySearchService.search(tId, new SearchRequestDTO());
 		});
 	}
 
